a4a898c1fd582d5909eb59e35c903b106b0f6633
Andres Aguirre <victor.aguirre@spaymentservices.com>
2023-03-14 17:26:41 -0600

    Corrutina

--- app/src/main/java/com/example/myapplication/TwoFragment.kt
+++ app/src/main/java/com/example/myapplication/TwoFragment.kt
@@ -8,6 +8,9 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.data.model.Projects
 import com.example.myapplication.ui.adapter.ProjectsAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class TwoFragment : Fragment(R.layout.fragment_two), ProjectsAdapter.OnProjectClickListener {
 
@@ -15,57 +18,65 @@ class TwoFragment : Fragment(R.layout.fragment_two), ProjectsAdapter.OnProjectCl
         super.onViewCreated(view, savedInstanceState)
         val recycler = requireActivity().findViewById<RecyclerView>(R.id.rv_list_project)
 
-        exampleListProject()
-        recycler.adapter = ProjectsAdapter(exampleListProject(), this@TwoFragment)
+        //exampleListProject()
+        exampleListProject {
+            recycler.adapter = ProjectsAdapter(it, this@TwoFragment)
+        }
     }
 
-    private fun exampleListProject(): List<Projects> {
+    private fun exampleListProject(callback: (result: List<Projects>) -> Unit) {
 
-        val project1 = Projects(
-            "San Lorenzo 1",
-            R.drawable.ic_launcher_background,
-            "150 departamentos",
-            "Av. Sanlorenzo s/n"
-        )
-        val project2 = Projects(
-            "San Lorenzo 2",
-            R.drawable.ic_launcher_background,
-            "40 departamentos",
-            "Av Sanlorenzo 1990"
-        )
-        val project3 = Projects(
-            "San Lorenzo 3",
-            R.drawable.ic_launcher_background,
-            "100 departamentos",
-            "Av Salorenzo 15640"
-        )
-        val project4 = Projects(
-            "Eugenia",
-            R.drawable.ic_launcher_background,
-            "15 departamentos",
-            "Metro Eugenia"
-        )
-        val project5 = Projects(
-            "Atlalilco",
-            R.drawable.ic_launcher_background,
-            "25 departamentos",
-            "Avenida Iztapalapa"
-        )
-        val project6 = Projects(
-            "Moctezuma",
-            R.drawable.ic_launcher_background,
-            "20 departamentos",
-            "Metro moctezuma"
-        )
+        GlobalScope.launch(Dispatchers.Main) {
 
-        return listOf(
-            project1,
-            project2,
-            project3,
-            project4,
-            project5,
-            project6
-        )
+            val project1 = Projects(
+                "San Lorenzo 1",
+                R.drawable.ic_launcher_background,
+                "150 departamentos",
+                "Av. Sanlorenzo s/n"
+            )
+            val project2 = Projects(
+                "San Lorenzo 2",
+                R.drawable.ic_launcher_background,
+                "40 departamentos",
+                "Av Sanlorenzo 1990"
+            )
+            val project3 = Projects(
+                "San Lorenzo 3",
+                R.drawable.ic_launcher_background,
+                "100 departamentos",
+                "Av Salorenzo 15640"
+            )
+            val project4 = Projects(
+                "Eugenia",
+                R.drawable.ic_launcher_background,
+                "15 departamentos",
+                "Metro Eugenia"
+            )
+            val project5 = Projects(
+                "Atlalilco",
+                R.drawable.ic_launcher_background,
+                "25 departamentos",
+                "Avenida Iztapalapa"
+            )
+            val project6 = Projects(
+                "Moctezuma",
+                R.drawable.ic_launcher_background,
+                "20 departamentos",
+                "Metro moctezuma"
+            )
+
+            callback(
+                listOf(
+                    project1,
+                    project2,
+                    project3,
+                    project4,
+                    project5,
+                    project6
+                )
+            )
+
+        }
 
     }
 
